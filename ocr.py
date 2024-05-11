0d7a74f211a56d4c0dc151701f964a7e2ada18cf
Mei Chen <meichen.mlc@gmail.com>
2024-05-11 14:56:33 -0700

    transcribe image

---
+++ ocr.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API')
+client = OpenAI(api_key=api_key)
+
+
+response = client.chat.completions.create(
+  model="gpt-4-turbo",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])
