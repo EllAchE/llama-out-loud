9f4ae648ce14f842431d857a7cdfceba4461dc87
EllAchE <26192612+EllAchE@users.noreply.github.com>
2024-05-11 14:35:37 -0700

    add groq

---
+++ groq.py
@@ -0,0 +1,19 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama3-8b-8192",
+)
+
+print(chat_completion.choices[0].message.content)
