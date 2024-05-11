211293fa423c5d7d6c521a5b54feae9b4fe06a0c
EllAchE <26192612+EllAchE@users.noreply.github.com>
2024-05-11 16:36:33 -0700

    add samples

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+- take image
+- convert image to text
+- get user input (hand or no hand)
+- send to groq, choose next step
+a. query brave for live data
+b. store passage
+c. store & share
+- respond to user with text confirmation of the action
+'''
