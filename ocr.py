93b54188549f72932564b56a9be80684924ec473
Mei Chen <meichen.mlc@gmail.com>
2024-05-11 15:58:41 -0700

    ocr done

--- ocr.py
+++ ocr.py
@@ -3,28 +3,29 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-api_key = os.getenv('OPENAI_API')
-client = OpenAI(api_key=api_key)
 
-url = "https://github.com/EllAchE/llama-out-loud/blob/ocr/image/book.jpeg?raw=true"
-
-response = client.chat.completions.create(
-  model="gpt-4-turbo",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "Transcribe what the book says"},
+def ocr(url):
+    api_key = os.getenv('OPENAI_API')
+    client = OpenAI(api_key=api_key)
+    response = client.chat.completions.create(
+    model="gpt-4-turbo",
+    messages=[
         {
-          "type": "image_url",
-          "image_url": {
-            "url": url,
-          },
-        },
-      ],
-    }
-  ],
-  max_tokens=300,
-)
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "Transcribe what the book says"},
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": url,
+            },
+            },
+        ],
+        }
+    ],
+    max_tokens=300,
+    )
+    return response.choices[0].message.content
 
-print(response.choices[0])
+if __name__ == "__main__":
+    print(ocr(url = "https://github.com/EllAchE/llama-out-loud/blob/ocr/image/zoom_book.png?raw=true"))
