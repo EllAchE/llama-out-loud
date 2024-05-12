15f3c1a0e6768584a9fb84eb6d2d4deac9be441a
EllAchE <26192612+EllAchE@users.noreply.github.com>
2024-05-11 22:17:00 -0700

    groq orchestrator

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import json
+
+from brave import brave_req, summarizer, concat_brave_search_results
+from groq_completion import groq_completion
+from groq_prompts import answer_question_prompt, entry_prompt
+
 '''
 - take image
 - convert image to text
@@ -10,24 +15,9 @@ c. store & share
 - respond to user with text confirmation of the action
 '''
 
-def grok_decision_tree(groq_res: str):
-    if ("BRAVE_SEARCH" in groq_res):
-        brave_res_obj = json.loads(groq_res)
-        brave_query = brave_res_obj["BRAVE_SEARCH"]
-        return "BRAVE_SEARCH", brave_query
-    elif ("STORE_PASSAGE" in groq_res):
-        store_passage = json.loads(groq_res)
-        return "STORE_PASSAGE", None
-    elif ("TRAINING_DATA" in groq_res):
-        training_data_obje = json.loads(groq_res)
-        res = training_data_obje["TRAINING_DATA"]
-        return "TRAINING_DATA", res
-    else:
-        raise ValueError("Invalid groq response")
-        return None
+def main(): 
+    question = "what books has shel silverstein written in the past year?"
 
-                                                         
-def main():
     shel_silverstein = '''MAGIC
  Sandra's seen a leprechaun,
  Eddie touched a troll,
@@ -38,29 +28,30 @@ def main():
  But all the magic I have known
  I've had to make myself'''
 
-    init_prompt = entry_prompt("what books has shel silverstein written in the past year?", shel_silverstein)
+    init_prompt = entry_prompt(question, shel_silverstein)
     groq_res = groq_completion(init_prompt)
 
     if ("BRAVE_SEARCH" in groq_res):
         brave_res_obj = json.loads(groq_res)
         brave_query = brave_res_obj["BRAVE_SEARCH"]
-        return "BRAVE_SEARCH", brave_query
+
+        # get results from brave
+        brave_response = brave_req(brave_query)
+        concatted_brave = concat_brave_search_results(brave_response)
+
+        # make the brave results useful before sending to voice endpoint
+        voice_response = answer_question_prompt(question, concatted_brave)
+
+        return "BRAVE_SEARCH", voice_response
     elif ("STORE_PASSAGE" in groq_res):
-        store_passage = json.loads(groq_res)
+        # here we should reference the most recent text data & use notion to store it
         return "STORE_PASSAGE", None
     elif ("TRAINING_DATA" in groq_res):
         training_data_obje = json.loads(groq_res)
         res = training_data_obje["TRAINING_DATA"]
+        # here we should immediately respond to the user based on what is in the training data
         return "TRAINING_DATA", res
     else:
         raise ValueError("Invalid groq response")
         return None
 
-    p = brave_req("what books has shel silverstein written in the past year?")
-
-    ss = concat_brave_search_results(p)
-
-    thep = answer_question_prompt(ss, "what books has shel silverstein written in the past year?")
-
-    groq_res = groq_completion(thep)
-    print(groq_res)
