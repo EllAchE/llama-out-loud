b9cfc49267593219955afd2b7b4571c1f949614d
EllAchE <26192612+EllAchE@users.noreply.github.com>
2024-05-11 21:51:29 -0700

    notion api working

--- notion.py
+++ notion.py
@@ -1,37 +1,55 @@
 import os
+from datetime import datetime
 from notion_client import Client
 
 nc = Client(auth=os.getenv("NOTION_API_KEY"))
 
 root_page = 'af999b3e02e0402e9698ddc4bcf81729'
 
-def create_notion_page():
+
+
+def create_notion_page(title, content, heading = "Summary"):
     # Create a new page in the specified database
-    new_page = nc.pages.create(**{
-        "parent": {"page_id": root_page},
-        "properties": {
-            "Name": {
-                "title": [
-                    {
-                        "text": {
-                            "content": "Your Page Title"
-                        }
-                    }
-                ]
-            },
-            "Description": {
-                "rich_text": [
-                    {
-                        "text": {
-                            "content": "Your description here"
-                        }
-                    }
-                ]
-            }
-            # Add more properties here if your database has more columns
-        }
-    })
+    page_data = {
+	"parent": { "page_id": root_page },
+  "icon": {
+  	"emoji": "🚀"
+  },
+	"properties": {
+			"title": [
+				{
+					"text": {
+						"content": title
+					}
+				}
+			],
+	},
+	"children": [
+		{
+			"object": "block",
+			"type": "heading_2",
+			"heading_2": {
+				"rich_text": [{ "type": "text", "text": { "content": heading } }]
+			}
+		},
+		{
+			"object": "block",
+			"type": "paragraph",
+			"paragraph": {
+				"rich_text": [
+					{
+						"type": "text",
+						"text": {
+							"content": content,
+						}
+					}
+				]
+			}
+		}
+	]
+}
+    new_page = nc.pages.create(**page_data)
 
     print("New page created with ID:", new_page["id"])
 
-create_notion_page()
+create_notion_page("My Page", "A summary of the page content")
