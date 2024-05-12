85fd2620b36d7d1a62bdf7600bc3a2b1b4c66997
Mei Chen <meichen.mlc@gmail.com>
2024-05-11 22:29:43 -0700

    up

--- server/up.py
+++ server/up.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 import os
+import sys
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
-
+sys.path.append("../")
+from ocr import ocr
 
 app = Flask(__name__)
 UPLOAD_FOLDER = '/Users/mlc/Code/hackathon/tester'
@@ -16,22 +18,18 @@ def upload_file():
         return "No file part", 400
     
     file = request.files['img']
-    # if file.filename == '':
-    #     return "No selected file", 400
+    
     if file:
         timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
         _, file_extension = os.path.splitext(file.filename)
         filename = f"{timestamp}{file_extension}"
         # filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        ocr(url = "https://github.com/EllAchE/llama-out-loud/blob/ocr/image/zoom_book.png?raw=true")
+
         return "File successfully uploaded", 200
-        counter += 1
-        # _, file_extension = os.path.splitext(file.filename)
-        # # Create filename using only the counter
-        # filename = secure_filename(f"{counter}{file_extension}")
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # counter += 1  # Increment the counter after saving the file
-        # return "File successfully uploaded", 200
+        
     
 
 if __name__ == '__main__':
