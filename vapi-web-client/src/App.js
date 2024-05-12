7403a5675231501e547847dc71d35fbd164d25a1
Justin Lee <justinlee38@outlook.com>
2024-05-11 17:46:29 -0700

    updated yapi-frontend

--- vapi-web-client/src/App.js
+++ vapi-web-client/src/App.js
@@ -1,33 +1,13 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Vapi from "@vapi-ai/web";
 
 function App() {
   const [message, setMessage] = useState("");
-
-  // Create a ref for the Vapi instance
   const vapiRef = React.useRef(null);
 
   useEffect(() => {
-    // Initialize the Vapi instance only once when the component mounts
     vapiRef.current = new Vapi("8aa55f4a-50ea-4ceb-b3f7-cef74ef7459e");
-
-    // vapi.on("speech-start", () => {
-    //   console.log("Assistant speech has started.");
-    // });
-
-    // vapi.on("speech-end", () => {
-    //   console.log("Assistant speech has ended.");
-    // });
-
-    // vapi.on("call-start", () => {
-    //   console.log("Call has started.");
-    // });
-
-    // vapi.on("call-end", () => {
-    //   console.log("Call has ended.");
-    // });
 
     const assistantOverrides = {
       transcriber: {
@@ -46,18 +26,12 @@ function App() {
       assistantOverrides
     );
 
-    // Cleanup function to possibly disconnect or clean up the Vapi instance when the component unmounts
     return () => {
-      if (vapiRef.current) {
-        // Perform cleanup if needed, such as disconnecting the Vapi session
-      }
+      // Cleanup if necessary
     };
-  }, []); // The empty dependency array ensures this effect runs only once
+  }, []);
 
   const handleClick = () => {
-    console.log("Sending message:");
-    console.log(message);
-
     if (vapiRef.current) {
       vapiRef.current.send({
         type: "add-message",
@@ -70,28 +44,19 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center">
         <input
           type="text"
+          className="form-control mb-2"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           placeholder="Enter your message"
         />
-        <button onClick={handleClick}>Send Message</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button className="btn btn-primary" onClick={handleClick}>
+          Send Message
+        </button>
+      </div>
     </div>
   );
 }
